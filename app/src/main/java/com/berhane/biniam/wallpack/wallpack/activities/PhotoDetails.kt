62df590ff501ffb48c037df81c6c40388e5b7fa8
Biniam Haddish <benhaddish@gmail.com>
2018-09-21 12:46:52 +0300

    fixed the brokrn detailed layout

--- app/src/main/java/com/berhane/biniam/wallpack/wallpack/activities/PhotoDetails.kt
+++ app/src/main/java/com/berhane/biniam/wallpack/wallpack/activities/PhotoDetails.kt
@@ -9,20 +9,17 @@ package com.berhane.biniam.wallpack.wallpack.activities
 import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toolbar
 import com.berhane.biniam.wallpack.wallpack.R
 import com.berhane.biniam.wallpack.wallpack.model.data.Photos
 import com.bumptech.glide.Glide
 import com.bumptech.glide.Priority
 import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.target.DrawableImageViewTarget
 import com.google.gson.Gson
-import kotlinx.android.synthetic.main.detailed_photo.*
+import java.text.NumberFormat
+import java.util.*
 
 class PhotoDetails : AppCompatActivity() {
 
@@ -32,32 +29,32 @@ class PhotoDetails : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.detailed_photo)
         context = this
+
         val imageView = findViewById<ImageView>(R.id.image_detailed)
         val photographerImg = findViewById<ImageView>(R.id.photographerIMGView)
         val photographerName = findViewById<TextView>(R.id.photographerName)
-//        val detailed_like_btn = findViewById<ImageButton>(R.id.detailed_like_btn)
+        val like_details_btn = findViewById<ImageButton>(R.id.like_details_btn)
         val detailed_user_location = findViewById<TextView>(R.id.detailed_user_location)
-//        val published_date = findViewById<TextView>(R.id.published_date)
-//        val tvLikes = findViewById<TextView>(R.id.tvLikes)
-//        val tvDownloads = findViewById<TextView>(R.id.tvDownloads)
-//        val tvColor = findViewById<TextView>(R.id.tvColor)
+        val published_date = findViewById<TextView>(R.id.published_date)
+        val likes_details = findViewById<TextView>(R.id.likes_details)
+        val details_download = findViewById<TextView>(R.id.details_download)
+        val color_details = findViewById<TextView>(R.id.color_details)
 
 
         val photos: Photos = Gson().fromJson<Photos>(intent.getStringExtra("Photo"), Photos::class.java)
-//            display metrics
-
-
-        val displayMetrics = (context as PhotoDetails).getResources().getDisplayMetrics()
-        val imageHeight: Float = displayMetrics.widthPixels / (photos.width.toFloat() / photos.height.toFloat())
 
         photographerName.text = "By " + photos.user.first_name
         detailed_user_location.text = photos.user.location
+        published_date.text = photos.created_at.split("T")[0]
+        likes_details.text=  NumberFormat.getInstance(Locale.US).format(photos.likes)
+       // details_download.text=photos.
+        color_details.text=photos.color
 
         // load the image here
         Glide.with(this@PhotoDetails)
-                .load(photos.urls.regular)
+                .load(photos.urls.small)
                 .apply(RequestOptions().priority(Priority.HIGH)
-                        .placeholder(R.drawable.placeholder))
+                        .placeholder(R.drawable.ic_placeholder))
                 .into(imageView)
 
 
@@ -67,7 +64,7 @@ class PhotoDetails : AppCompatActivity() {
                         .placeholder(R.drawable.ic_account_circle))
                 .into(photographerImg)
 
-        Log.d(TAG, "userImage" + photos.user.profile_image.medium)
+
     }
 
 }
